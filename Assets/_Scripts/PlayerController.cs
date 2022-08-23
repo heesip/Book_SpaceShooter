591ac9848504155188d6b33339f631c068b8bc06
HeeSeop Kim <105268687+heesip@users.noreply.github.com>
2022-08-23 20:33:11 +0900

    171p

--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -1,24 +1,55 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    Animation anim;
     [SerializeField] float moveSpeed = 10.0f;
+    [SerializeField] float turnSpeed = 80.0f;
+
+
     private void Start()
     {
+        anim = GetComponent<Animation>();
+        anim.Play("Idle");
     }
     void Update()
     {
         float h = Input.GetAxisRaw("Horizontal");
         float v = Input.GetAxisRaw("Vertical");
-
-        //Debug.Log("h = " + h);
-        //Debug.Log("v = " + v);
-        //transform.position += new Vector3(h, 0, v).normalized;
+        float r = Input.GetAxisRaw("Mouse X");
 
         Vector3 moveDir = (Vector3.forward * v) + (Vector3.right * h);
+
         transform.Translate(moveSpeed * Time.deltaTime * moveDir.normalized);
 
+        transform.Rotate(turnSpeed * Time.deltaTime * r * Vector3.up);
+
+        PlayerAnim(h, v);
+    }
+    private void PlayerAnim(float h, float v)
+    {
+        if (v >= 0.1f)
+        {
+            anim.CrossFade("RunF", 0.25f);
+        }
+        else if (v <= -0.1f)
+        {
+            anim.CrossFade("RunB", 0.25f);
+        }
+        else if(h >= 0.1f)
+        {
+            anim.CrossFade("RunR", 0.25f);
+        }
+        else if(h <= -0.1f)
+        {
+            anim.CrossFade("RunL", 0.25f);
+        }
+        else
+        {
+            anim.CrossFade("Idle", 0.25f);
+        }
     }
 }
