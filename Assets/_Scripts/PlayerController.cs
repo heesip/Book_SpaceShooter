a584e72273bd72acd733914e5099616636c51a4f
HeeSeop Kim <khs37705@naver.com>
2022-08-23 17:04:27 +0900

    142p

---
+++ Assets/_Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 10.0f;
+    private void Start()
+    {
+    }
+    void Update()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        //Debug.Log("h = " + h);
+        //Debug.Log("v = " + v);
+        //transform.position += new Vector3(h, 0, v).normalized;
+
+        transform.Translate(Vector3.forward * Time.deltaTime * v * moveSpeed);
+    }
+}
